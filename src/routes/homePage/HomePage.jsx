d1f0304b6bfa3e643303d8f4eac0d33c39a281a4
Suraj Somani <surajsomani2002@gmail.com>
2024-08-06 15:04:22 +0530

    Home page design done with animation

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -4,7 +4,21 @@ import "./homePage.css"
 const HomePage = () => {
   return (
     <div className="homePage">
-      <Link to="/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1>SURAJ AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam error cumque suscipit.</h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+        </div>
+      </div>
     </div>
   )
 }
